17ecf4094aa266f0cefc74b001836245b965ab43
Andrés Ariza <andres-ariza70@hotmail.com>
2023-03-08 17:50:39 -0500

    first commit

---
+++ src/main/java/edu/eci/arep/LogService.java
@@ -0,0 +1,81 @@
+package edu.eci.arep;
+
+import com.google.gson.Gson;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
+
+import static spark.Spark.*;
+
+public class LogService {
+
+    private static MongoClient mongo;
+
+    public static void main(String[] args) {
+
+        port(getPort());
+
+        get("/", (req, res) -> {
+            res.type("application/json");
+            Gson gson = new Gson();
+            return gson.toJson(getLogs());
+        });
+
+        post("/", (req, res) -> {
+            res.type("application/json");
+            insertLog(req.body());
+            Gson gson = new Gson();
+            return gson.toJson(getLogs());
+        });
+
+    }
+
+    public static List<Document> getLogs() {
+        mongo = new MongoClient("192.168.5.111", 27017);
+        MongoDatabase db = mongo.getDatabase("admin");
+        MongoCollection<Document> collection = db.getCollection("Logs");
+
+        List<Document> documents = new ArrayList<>();
+        try (MongoCursor<Document> cursor = collection.find().sort(Sorts.descending("createdAt")).iterator()) {
+            while (cursor.hasNext()) {
+                documents.add(cursor.next());
+            }
+        }
+
+        System.out.println(documents + "\n");
+        mongo.close();
+        return documents;
+    }
+
+    public static void insertLog(String body) {
+        mongo = new MongoClient( "192.168.5.111" , 27017);
+        MongoDatabase db = mongo.getDatabase("admin");
+
+        MongoCollection<Document> collection = db.getCollection("Logs");
+
+        System.out.println(body);
+        Document document = Document.parse(body);
+        System.out.println("POSTEANDO");
+        document.append("createdAt", new Date());
+
+        collection.insertOne(document);
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 35001;
+    }
+}
